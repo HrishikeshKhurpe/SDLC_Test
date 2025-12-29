9d3534084e2c372da4423ce56e1cd9d7f01b5cc3
HrishikeshKhurpe <hrishikeshkhurpe@gmail.com>
2025-12-29 19:25:28 +0530

    feat: DL-1
    File: src/features/calculator/tests/test_calculator.py

--- src/features/calculator/tests/test_calculator.py
+++ src/features/calculator/tests/test_calculator.py
@@ -1,4 +1,3 @@
-""
 import unittest
 from src.features.calculator.calculator import Hrishikesh_Calculator
 
@@ -7,26 +6,32 @@ class TestHrishikeshCalculator(unittest.TestCase):
         self.calculator = Hrishikesh_Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calculator.add(5, 3), 8)
-        self.assertEqual(self.calculator.add(-2, 4), 2)
+        self.assertEqual(self.calculator.add(2, 3), 5)
+        self.assertEqual(self.calculator.add(-2, 3), 1)
         self.assertEqual(self.calculator.add(0, 0), 0)
+        self.assertRaises(ValueError, self.calculator.add, "a", 3)
+        self.assertRaises(ValueError, self.calculator.add, 2, "b")
 
     def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(10, 4), 6)
-        self.assertEqual(self.calculator.subtract(0, 5), -5)
-        self.assertEqual(self.calculator.subtract(7, 7), 0)
+        self.assertEqual(self.calculator.subtract(5, 3), 2)
+        self.assertEqual(self.calculator.subtract(3, 5), -2)
+        self.assertEqual(self.calculator.subtract(0, 0), 0)
+        self.assertRaises(ValueError, self.calculator.subtract, "a", 3)
+        self.assertRaises(ValueError, self.calculator.subtract, 2, "b")
 
     def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(2, 6), 12)
-        self.assertEqual(self.calculator.multiply(-3, 4), -12)
-        self.assertEqual(self.calculator.multiply(0, 100), 0)
+        self.assertEqual(self.calculator.multiply(2, 3), 6)
+        self.assertEqual(self.calculator.multiply(-2, 3), -6)
+        self.assertEqual(self.calculator.multiply(0, 0), 0)
+        self.assertRaises(ValueError, self.calculator.multiply, "a", 3)
+        self.assertRaises(ValueError, self.calculator.multiply, 2, "b")
 
     def test_divide(self):
-        self.assertEqual(self.calculator.divide(15, 3), 5)
-        self.assertEqual(self.calculator.divide(10, 2), 5)
-        with self.assertRaises(ZeroDivisionError):
-            self.calculator.divide(10, 0)
+        self.assertEqual(self.calculator.divide(6, 3), 2)
+        self.assertEqual(self.calculator.divide(3, 6), 0.5)
+        self.assertRaises(ValueError, self.calculator.divide, 6, 0)
+        self.assertRaises(ValueError, self.calculator.divide, "a", 3)
+        self.assertRaises(ValueError, self.calculator.divide, 2, "b")
 
 if __name__ == '__main__':
     unittest.main()
-""
